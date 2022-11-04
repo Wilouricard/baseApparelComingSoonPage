687e5248c0cf4530d18d584188be0e037d1424a1
Nam Tang <nam.tang@live.fr>
2022-11-04 09:01:53 +0100

    body min-h changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     colors: {
       'Desaturated-Red': 'hsl(0, 36%, 70%)',
       'Soft-Red': 'hsl(0, 93%, 68%)',
-      'Dark Grayish-Red': 'hsl(0, 6%, 24%)',
+      'Dark-Grayish-Red': 'hsl(0, 6%, 24%)',
       'nav-color': 'hsl(0, 100%, 98%)',
       'red': 'hsl(0, 100%, 50%)',
     }
